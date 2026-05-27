97d941bd1497b86b9e3ee6ecf00134b3e61f89c7
Sumitha Sekar <98888424+sumi142@users.noreply.github.com>
2026-05-27 06:52:46 +0000

    clear

--- Task_2/API.py
+++ Task_2/API.py
@@ -2,10 +2,20 @@ import requests
 import pandas as pd
 import datetime as dt
 from datetime import date
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logging.info("Starting the API data retrieval process")
 
 url = "https://archive-api.open-meteo.com/v1/era5?latitude=52.52&longitude=13.41&start_date=2021-01-01&end_date=2021-12-31&hourly=temperature_2m"
 
 response = requests.get(url)
+response.raise_for_status()
+logging.info("API request successful")
 data = response.json()
 #print(data)
 
@@ -15,6 +25,7 @@ df=pd.DataFrame(data=hourly)
 df["time"] = pd.to_datetime(df["time"])
 print(df)
 
+logging.info("Checking null values")
 print(df.isnull().sum())
 
 df['date']=df['time'].dt.date
